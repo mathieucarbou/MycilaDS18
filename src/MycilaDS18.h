2a675ae1058bd443e03e90f3af7becea2594bd65
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-01-27 09:36:47 +0100

    v4.1.4

--- src/MycilaDS18.h
+++ src/MycilaDS18.h
@@ -20,10 +20,10 @@
 #include <mutex>
 #include <optional>
 
-#define MYCILA_DS18_VERSION          "4.1.3"
+#define MYCILA_DS18_VERSION          "4.1.4"
 #define MYCILA_DS18_VERSION_MAJOR    4
 #define MYCILA_DS18_VERSION_MINOR    1
-#define MYCILA_DS18_VERSION_REVISION 3
+#define MYCILA_DS18_VERSION_REVISION 4
 
 // If the temperature is changing from less than 0.3 degrees, we consider it has not changed, to avoid too many updates
 // Example:
